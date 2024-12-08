f47884c08e02860f4444d8187be8636c543d6512
houssam chakir <thisischchannel@gmail.com>
2024-12-08 11:16:56 +0100

    Loading effect and error handling

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,13 +1,18 @@
 import Header from "./Header"
 import CartOverview from '../features/cart/CartOverview'
-import { Outlet } from "react-router-dom"
+import { Outlet, useNavigation } from "react-router-dom"
+import Loading from "./Loading"
 
 function AppLayout() {
+  const navigation = useNavigation()
+  const isLoading = navigation.state === 'loading'
+
   return (
-    <div>
+    <div className="layout">
       <Header />
 
       <main>
+        {isLoading && <Loading />}
         <Outlet />
       </main>
 
