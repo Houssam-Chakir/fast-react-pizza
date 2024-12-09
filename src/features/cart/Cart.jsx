b479a03e5474c2cdcd0b64c81fc8d3c474a4cc85
houssam chakir <thisischchannel@gmail.com>
2024-12-09 23:19:27 +0100

    adding and deleting items to/from cart and menu

--- src/features/cart/Cart.jsx
+++ src/features/cart/Cart.jsx
@@ -1,27 +1,29 @@
-import { Link } from 'react-router-dom';
-import LinkButton from '../../ui/LinkButton';
-import Button from '../../ui/Button';
-import CartItem from './CartItem';
-import { useSelector } from 'react-redux';
+import { Link } from "react-router-dom";
+import LinkButton from "../../ui/LinkButton";
+import Button from "../../ui/Button";
+import CartItem from "./CartItem";
+import EmptyCart from './EmptyCart'
+import { useDispatch, useSelector } from "react-redux";
+import { clearCart } from "./cartSlice";
 
 const fakeCart = [
   {
     pizzaId: 12,
-    name: 'Mediterranean',
+    name: "Mediterranean",
     quantity: 2,
     unitPrice: 16,
     totalPrice: 32,
   },
   {
     pizzaId: 6,
-    name: 'Vegetale',
+    name: "Vegetale",
     quantity: 1,
     unitPrice: 13,
     totalPrice: 13,
   },
   {
     pizzaId: 11,
-    name: 'Spinach and Mushroom',
+    name: "Spinach and Mushroom",
     quantity: 1,
     unitPrice: 15,
     totalPrice: 15,
@@ -29,9 +31,11 @@ const fakeCart = [
 ];
 
 function Cart() {
-  const username = useSelector(state => state.user.username)
+  const dispatch = useDispatch()
+  const username = useSelector((state) => state.user.username);
+  const cart = useSelector((state) => state.cart.cart);
 
-  const cart = fakeCart;
+  if(!cart.length) return <EmptyCart />
 
   return (
     <div className="px-4 py-3">
@@ -41,7 +45,7 @@ function Cart() {
 
       <ul className="mt-3 divide-y divide-stone-200 border-b">
         {cart.map((item) => (
-          <CartItem item={item} key={item.key} />
+          <CartItem item={item} key={item.pizzaId} />
         ))}
       </ul>
 
@@ -50,7 +54,7 @@ function Cart() {
           Order pizzas
         </Button>
 
-        <Button type="secondary">Clear cart</Button>
+        <Button type="secondary" onClick={() => dispatch(clearCart())}>Clear cart</Button>
       </div>
     </div>
   );
